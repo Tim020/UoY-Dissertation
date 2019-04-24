Fix for zero speed variance

VehicleGarage.py
@@ -16,22 +16,32 @@ class Garage(object):
         self._car_pct = car_pct
         car_min = (1 - (car_speed_variance / 100))
         car_max = (1 + (car_speed_variance / 100))
-        car_std = ((car_speed * car_max) - (car_speed * car_min)) / 4
-        self._car_velocities = stats.truncnorm(
-            ((car_speed * car_min) - car_speed) / car_std,
-            ((car_speed * car_max) - car_speed) / car_std,
-            loc=car_speed, scale=car_std)
+        if car_speed_variance > 0:
+            car_std = ((car_speed * car_max) - (car_speed * car_min)) / 4
+            self._car_velocities = stats.truncnorm(
+                ((car_speed * car_min) - car_speed) / car_std,
+                ((car_speed * car_max) - car_speed) / car_std,
+                loc=car_speed, scale=car_std)
+        else:
+            self._car_velocities = stats.uniform(
+                loc=(car_speed * car_min),
+                scale=(car_speed * car_max) - car_speed)
         self._car_velocities.random_state = np.random.RandomState(
             seed=short_seed)
 
         self._truck_pct = truck_pct
         truck_min = (1 - (truck_speed_variance / 100))
         truck_max = (1 + (truck_speed_variance / 100))
-        truck_std = ((truck_speed * truck_max) - (truck_speed * truck_min)) / 4
-        self._truck_velocities = stats.truncnorm(
-            ((truck_speed * truck_min) - truck_speed) / truck_std,
-            ((truck_speed * truck_max) - truck_speed) / truck_std,
-            loc=truck_speed, scale=truck_std)
+        if truck_speed_variance > 0:
+            truck_std = ((truck_speed * truck_max) - (truck_speed * truck_min)) / 4
+            self._truck_velocities = stats.truncnorm(
+                ((truck_speed * truck_min) - truck_speed) / truck_std,
+                ((truck_speed * truck_max) - truck_speed) / truck_std,
+                loc=truck_speed, scale=truck_std)
+        else:
+            self._truck_velocities = stats.uniform(
+                loc=(truck_speed * truck_min),
+                scale=(truck_speed * truck_max) - truck_speed)
         self._truck_velocities.random_state = np.random.RandomState(
             seed=short_seed)
 